321da1e113aabf564c080c8e31c0e35bb4ec88e5
Paweł Drabarek <01151568@pw.edu.pl>
2022-03-20 23:03:59 +0100

    basic setup

--- imagePreparer.py
+++ imagePreparer.py
@@ -1,3 +1,9 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
 def main():
     print("Hello world")
 
