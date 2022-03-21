92f0777d7cea2d849f7658f3d5192e829bb23045
Paweł Drabarek <01151568@pw.edu.pl>
2022-03-21 15:55:52 +0100

    getFolderPAth init

--- imagePreparer.py
+++ imagePreparer.py
@@ -1,10 +1,13 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets
+import torchvision
 from torchvision.transforms import ToTensor
 
+def getFolderPath():
+    print("_PATH")
+
 def main():
-    print("Hello world")
+    getFolderPath()
 
 main()
